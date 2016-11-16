c2d42523b98e96f062b074262da7c672f31b520e
victor rodriguez <victorR@victorrodriguez.local>
2016-11-15 16:20:15 -0800

    Initial Commit

---
+++ tabManTests/tabManTests.swift
@@ -0,0 +1,36 @@
+//
+//  tabManTests.swift
+//  tabManTests
+//
+//  Created by victor rodriguez on 11/15/16.
+//  Copyright © 2016 Victor Rodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import tabMan
+
+class tabManTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
